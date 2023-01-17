beb317c703694bb7d890bf340239b7d5814aca0f
Olatunde Lawrence <93929624+Lawrence172@users.noreply.github.com>
2023-01-17 13:27:35 +0100

    update on pokemon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import { getAllPokemonList } from "./api/pokemon";
 
 function App() {
+  const [pokemonData,setPokemonData] = useState([]);
+
+
+  useEffect(() => {
+    async function fetchData(){
+      const data = await getAllPokemonList();
+      setPokemonData(data?.result);
+    }
+    fetchData();
+  }, [])
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div>
+        {pokemonData?.map((poke,i) =>{
+          return(
+            <div>
+              <h1>Hello world</h1>
+            </div>
+          )
+        })}
+      </div>
           </div>
   );
 }
