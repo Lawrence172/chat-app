a8680a1ffe8642e359a474c4125c808023dbf2a0
Olatunde Lawrence <93929624+Lawrence172@users.noreply.github.com>
2023-02-22 13:48:47 +0100

    New update

--- src/Component/Signin/Signin.js
+++ src/Component/Signin/Signin.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import goggle from '../../asset/img/goggle.svg'
+import { Link } from 'react-router-dom'
 
-function Signin() {
+function Signup() {
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+      <div className='signup'>
+        <h1>Sign Up</h1>
+        <div className='form'>
+          <input type='text' placeholder='First Name' />
+          <input type='text' placeholder='Last Name' />
+          <input type='email' placeholder='Email Address' />
+          <input type='password' placeholder='Password' />
+          <button>Sign Up</button>
+          <div className='or'>
+            <hr />
+            Or
+            <hr />
+          </div>
+          <div className='signup-options'>
+            <div className='option'>
+              <img src={goggle} alt='' />
+              <h6>Authorize with Goggle</h6>
+            </div>
+          </div>
+          <div className='already'>
+            <h6>Already have an account?</h6>
+            <Link to='/Signin' className='sign'>Sign in</Link>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default Signin
+export default Signup
